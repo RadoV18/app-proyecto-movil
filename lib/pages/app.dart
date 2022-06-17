5b68c8a0a48e63808420614b4be05aa4e1ed9cbb
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-06-16 21:40:02 -0400

    Tabla funcionando

--- lib/pages/app.dart
+++ lib/pages/app.dart
@@ -9,6 +9,7 @@ import 'dart:async';
 
 import '../TextToSpeech.dart';
 import '../Speech.dart';
+import '../singletons/TableModel.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -18,11 +19,24 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+
   String text = 'Texto inicial';
   bool isListening = false;
   bool sendRequest = false;
   List<types.Message> _messages = [];
   final _user = const types.User(id: '06c33e8b-e835-4736-80f4-63f44b66666c');
+
+  bool tableFlag = false;
+
+  // Table
+  double cellWidth = 100.0;
+  double cellHeight = 40.0;
+  double margin = 0.5;
+
+  int counter = 1;
+
+  late TableModel tModel;
+  List<List<String>> table = [];
 
   @override
   void initState() {
@@ -56,21 +70,58 @@ class _AppState extends State<App> {
       _messages = messages;
     });
   }
+
   @override
   Widget build(BuildContext context) {
+    if(tableFlag) {
+      tModel = TableModel();
+      table = tModel.getTable();
+      return getTable();
+    }
+    return getChat();
+  }
+  Future toggleRecording() => Speech.toggleRecording(
+    onResult: (text) {
+        setState(() async {
+          if(!isListening && sendRequest) {
+            if(tableFlag) {
+              print(text);
+            } else {
+              _handleSendPressed(types.PartialText(text:text), '06c33e8b-e835-4736-80f4-63f44b66666c');
+              List<String> response = await assistantRequest(text);
+              for(int i = 0; i < response.length; i++) {
+                _handleSendPressed(types.PartialText(text:response[i]), 'server');
+                await speak(response[i]);
+              }
+              Speech.stop();
+              print(response);
+              if(response[0] == "Abriendo tabla...") {
+                setState(() {
+                  tableFlag = true;
+                });
+              }
+            }
+          }
+        }
+      );
+    },
+    onListening: (isListening) {
+      setState(() => this.isListening = isListening);
+    },
+  );
 
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Aplicación'),
-        backgroundColor: Color(0xff32746D),
-      ),
-      body: Chat(
-        messages: _messages,
-        onSendPressed: (message){},
-        user: _user,
-      ),
-      bottomNavigationBar: BottomAppBar(
+  Widget getChat() => Scaffold(
+    appBar: AppBar(
+      centerTitle: true,
+      title: Text('Aplicación'),
+      backgroundColor: Color(0xff32746D),
+    ),
+    body: Chat(
+      messages: _messages,
+      onSendPressed: (message){},
+      user: _user,
+    ),
+    bottomNavigationBar: BottomAppBar(
         color: Color(0xff32746D),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -87,30 +138,95 @@ class _AppState extends State<App> {
             ),
           ],
         )
-      ),
-    );
-  }
-  Future toggleRecording() => Speech.toggleRecording(
-    onResult: (text) {
-      setState(() async {
-        if(!isListening && sendRequest) {
-          _handleSendPressed(types.PartialText(text:text), '06c33e8b-e835-4736-80f4-63f44b66666c');
-          List<String> response = await assistantRequest(text);
-          for(int i = 0; i < response.length; i++) {
-            _handleSendPressed(types.PartialText(text:response[i]), 'server');
-            await speak(response[i]);
+    ),
+  );
+
+  Widget getTable() => Scaffold(
+    appBar: AppBar(
+      centerTitle: true,
+      title: Text('Tabla'),
+      backgroundColor: Color(0xff32746D),
+    ),
+    body: Container(
+        color: Colors.black,
+        child: SingleChildScrollView(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Flexible(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    //Numero de filas
+                    children: _buildTable(tModel.getRowCount(), tModel.getColumnCount()),
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+    ),
+    bottomNavigationBar: BottomAppBar(
+        color: Color(0xff32746D),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                toggleRecording();
+                sendRequest = true;
+              },
+              style: ElevatedButton.styleFrom(
+                primary: isListening  ? Colors.red : Color(0xff32746D),
+              ),
+              child: isListening  ? Icon(Icons.stop): Icon(Icons.mic),
+            ),
+          ],
+        )
+    ),
+  );
+
+  // Table
+  List<Widget> _buildTable(int rows, int columns) {
+    List<Widget> result = [];
+    for(int i = 0; i < rows; i++) {
+      List<Widget> cells = [];
+      for(int j = 0; j < columns; j++) {
+        if(i == 0) {
+          // header row
+          if(j == 0) {
+            // top left corner
+            cells.add(cell("-", true));
+          } else {
+            cells.add(cell(j.toString(), true));
           }
-          Speech.stop();
-          print(response);
-          if(response[0] == "Abriendo tabla...") {
-            Navigator.pushNamed(context, '/table');
-          }
+        } else if (j == 0) {
+          // first column
+          cells.add(cell(i.toString(), true));
+        } else {
+          // cell
+          cells.add(cell(table[i][j], false));
         }
       }
+      result.add(
+          Row( children: cells )
       );
-    },
-    onListening: (isListening) {
-      setState(() => this.isListening = isListening);
-    },
-  );
+    }
+    return result;
+  }
+
+  Container cell(String value, bool isHeader) {
+    return Container(
+      alignment: Alignment.center,
+      width: cellWidth,
+      height: cellHeight,
+      color: isHeader ? Colors.amber : Colors.white,
+      margin: EdgeInsets.all(margin),
+      //Texto de celdas
+      child: Text(value),
+    );
+  }
+
 }
+
